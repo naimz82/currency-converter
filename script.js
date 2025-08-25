c6a037da7da634d4e7e823e4ecc34f4eb85c2342
Naim Zulkipli <naim@amz.com.my>
2025-08-25 12:22:22 +0800

    Added event listeners

--- script.js
+++ script.js
@@ -42,4 +42,31 @@ function loadBaseCurrency(curr){
 
 $(document).ready(function(){
     loadBaseCurrency(defaultBaseCurrency);
+
+    $("#frmHomeCurrencyConverter").on("change", "#from_curr", function(){
+        let selectedCurr = $(this).val();
+        loadBaseCurrency(selectedCurr);
+    });
+
+    $("#frmHomeCurrencyConverter").on("change", "#into_curr", function(){
+        let selectedCurr = $(this).val();
+        let fromVal = parseFloat( $("#currencyFrom").val() );
+        let rate = exchanges[0][selectedCurr];
+        if(rate){
+            $("#convertedValue").val((fromVal * rate).toFixed(2));
+        } else {
+            $("#convertedValue").val("0.00");
+        }
+    });
+
+    $("#frmHomeCurrencyConverter").on("input", "#currencyFrom", function(){
+        let fromVal = parseFloat( $(this).val() );
+        let toCurr = $("#into_curr option:selected").val();
+        if(toCurr.length > 0 && !isNaN(fromVal)){
+            let rate = exchanges[0][toCurr];
+            $("#convertedValue").val((fromVal * rate).toFixed(2));
+        } else {
+            $("#convertedValue").val("0.00");
+        }
+    });
 });
